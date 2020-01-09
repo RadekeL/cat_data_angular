a0186a95fc6a3e533167a60d609b1f47168b0918
Radek <radeklejman123@gmail.com>
2020-01-10 00:14:13 +0100

    Implementation basic form version with basic authorisation

--- src/app/components/login/template-driven-form/template-driven-form.component.ts
+++ src/app/components/login/template-driven-form/template-driven-form.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-template-driven-form',
-  templateUrl: './template-driven-form.component.html',
-  styleUrls: ['./template-driven-form.component.scss']
+  selector: "app-template-driven-form",
+  templateUrl: "./template-driven-form.component.html",
+  styleUrls: ["./template-driven-form.component.scss"]
 })
 export class TemplateDrivenFormComponent implements OnInit {
+  user = new LogIn();
+  constructor() {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  onSubmit(contactForm) {
+    console.log(contactForm);
+    console.log(this.user);
   }
+}
 
+class LogIn {
+  constructor(public login?: string, public password?: string) {}
 }
