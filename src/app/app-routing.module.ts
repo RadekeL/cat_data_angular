2f4ee3c9c9531a9d2ca699805672371c0363a3d6
Radek <radeklejman123@gmail.com>
2020-01-11 12:48:16 +0100

    Implementation Page-not-found in router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,8 +3,14 @@ import { Routes, RouterModule } from "@angular/router";
 import { LoginComponent } from "./components/login/login.component";
 import { CatSearchComponent } from "./components/cat-search/cat-search.component";
 import { CatDataEditorComponent } from "./components/cat-data-editor/cat-data-editor.component";
+import { PageNotFoundComponent } from "./components/page-not-found/page-not-found.component";
 
 const appRoutes: Routes = [
+  {
+    path: "",
+    redirectTo: "/login",
+    pathMatch: "full"
+  },
   {
     path: "login",
     component: LoginComponent
@@ -16,6 +22,10 @@ const appRoutes: Routes = [
   {
     path: "edit",
     component: CatDataEditorComponent
+  },
+  {
+    path: "**",
+    component: PageNotFoundComponent
   }
 ];
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from "./app.component";
 import { LoginComponent } from "./components/login/login.component";
 import { CatSearchComponent } from "./components/cat-search/cat-search.component";
 import { CatDataEditorComponent } from "./components/cat-data-editor/cat-data-editor.component";
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     CatSearchComponent,
-    CatDataEditorComponent
+    CatDataEditorComponent,
+    PageNotFoundComponent
   ],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
