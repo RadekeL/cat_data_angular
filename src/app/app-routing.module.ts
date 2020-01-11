a2a62ff888a5ca9862e7a3a2f00588d8945bd854
Radek <radeklejman123@gmail.com>
2020-01-11 12:35:48 +0100

    Create routing in app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,25 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { LoginComponent } from "./components/login/login.component";
+import { CatSearchComponent } from "./components/cat-search/cat-search.component";
+import { CatDataEditorComponent } from "./components/cat-data-editor/cat-data-editor.component";
 
-
-const routes: Routes = [];
-
+const appRoutes: Routes = [
+  {
+    path: "login",
+    component: LoginComponent
+  },
+  {
+    path: "search",
+    component: CatSearchComponent
+  },
+  {
+    path: "edit",
+    component: CatDataEditorComponent
+  }
+];
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(appRoutes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,17 @@ import { NgModule } from "@angular/core";
 import { HttpClientModule } from "@angular/common/http";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
-import { LoginComponent } from './components/login/login.component';
-import { CatSearchComponent } from './components/cat-search/cat-search.component';
-import { CatDataEditorComponent } from './components/cat-data-editor/cat-data-editor.component';
+import { LoginComponent } from "./components/login/login.component";
+import { CatSearchComponent } from "./components/cat-search/cat-search.component";
+import { CatDataEditorComponent } from "./components/cat-data-editor/cat-data-editor.component";
 
 @NgModule({
-  declarations: [AppComponent, LoginComponent, CatSearchComponent, CatDataEditorComponent],
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    CatSearchComponent,
+    CatDataEditorComponent
+  ],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent]
